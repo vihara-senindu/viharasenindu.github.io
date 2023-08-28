04b315a99d607a85f1271e897d3f62a377050e86
viharasenindu <viharasenindu22@gmail.com>
2023-08-28 20:18:48 +0530

    Updates

---
+++ precache-manifest.ed0f0832a4026f09c100e0edae349d33.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10da44884b606fc05b872ac8103cb817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0efeb94eb7daae99591",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "caf175bf3e343a2ee051",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "5fb19a791ad5d807ef3b",
+    "url": "/static/css/main.8a6c0df2.chunk.css"
+  },
+  {
+    "revision": "8a92f96348543cb4a80e",
+    "url": "/static/js/2.398db563.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.398db563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0efeb94eb7daae99591",
+    "url": "/static/js/3.c71e97f5.chunk.js"
+  },
+  {
+    "revision": "caf175bf3e343a2ee051",
+    "url": "/static/js/4.6fe9fb3e.chunk.js"
+  },
+  {
+    "revision": "5fb19a791ad5d807ef3b",
+    "url": "/static/js/main.1a265d6c.chunk.js"
+  },
+  {
+    "revision": "98537d515a7ebfcef809",
+    "url": "/static/js/runtime-main.cb40bc7b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f02965d96c9e02967dd95f87fd0e9598",
+    "url": "/static/media/UI.f02965d9.png"
+  },
+  {
+    "revision": "7308af643b6b827b0aeeffbda6231bb9",
+    "url": "/static/media/coffe.7308af64.png"
+  },
+  {
+    "revision": "bc60af580141a47983cc9b18bbc044a3",
+    "url": "/static/media/game.bc60af58.png"
+  },
+  {
+    "revision": "aa4910f017f0e723f5f79bd42c7b4388",
+    "url": "/static/media/github.aa4910f0.jpg"
+  },
+  {
+    "revision": "f6f73f37f1077862f5a70895c305783d",
+    "url": "/static/media/graphic.f6f73f37.png"
+  },
+  {
+    "revision": "6ea544b093cb3611c68faf573141d68b",
+    "url": "/static/media/graphics.6ea544b0.png"
+  },
+  {
+    "revision": "f2026d1e6683e7bf3c5b60f8dbec1c23",
+    "url": "/static/media/iot.f2026d1e.png"
+  },
+  {
+    "revision": "34d072ba3d6f6feb7a4622b0f3940ff5",
+    "url": "/static/media/mcc.34d072ba.png"
+  },
+  {
+    "revision": "e039cb2c7d634f6b3fa6ab59feffd224",
+    "url": "/static/media/profeciency.e039cb2c.svg"
+  },
+  {
+    "revision": "f4bcdc51a3fb581c57fd4fae9807f780",
+    "url": "/static/media/skills.f4bcdc51.svg"
+  },
+  {
+    "revision": "bb0d5fd053a4a87402d936e997d19ca7",
+    "url": "/static/media/stc.bb0d5fd0.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b59c78881c8af67798342ce45cdee98b",
+    "url": "/static/media/uom.b59c7888.png"
+  },
+  {
+    "revision": "dbd41a00e3b43e027ed6eaab903de3d1",
+    "url": "/static/media/vihara.dbd41a00.png"
+  }
+]);
